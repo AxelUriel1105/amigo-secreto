e16e2059ebeec81b3a8436133f9d9108591e1032
Axel <axl.uriel15414@gmail.com>
2025-01-22 13:12:18 -0600

    crear funcion agregarAmigo, validar la entrada y limpiar la caja

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo.length)
+    if (amigo.length != 0){
+        amigos.push(amigo)
+        console.log(amigos);
+    }else{
+        alert('Por favor, inserte un nombre.');
+
+    }
+    limpiarCaja();
+    return;
+}
+function agregarAmigosHTML(ListaAmigos){
+
+}
+function limpiarCaja(){
+    let valorCaja = document.querySelector('#amigo').value = '';
+    return;
+}
